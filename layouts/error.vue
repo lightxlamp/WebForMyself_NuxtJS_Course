e9f2aea537ea07c031ef20e90ce2eccbc21850e1
lightxlamp <sheveliukhin.s@gmail.com>
2020-05-12 11:09:36 +0600

    feat: Add "Error page"

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="error-layout">
+        <div class="error">
+            <h1 class="error__title">Error  404</h1>
+            <nuxt-link to="/">Back to Home Page</nuxt-link>
+        </div> 
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .error-layout {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;  
+    }
+
+    .error {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 4rem;
+
+        &__title {
+            color: rgb(145, 5, 5 );
+            margin-bottom: 1rem;
+        }
+    }
+</style>
