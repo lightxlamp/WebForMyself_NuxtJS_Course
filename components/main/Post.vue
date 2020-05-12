45d74299841ff45bcaeaa77b652aa33a9c0d9e67
lightxlamp <sheveliukhin.s@gmail.com>
2020-05-12 10:18:12 +0600

    feat: Complete "Post" component. Add styles / markup / and test event

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,19 +1,68 @@
 <template>
-    <article class="post">
+    <el-card
+        shadow="hover"
+        :body-style="{ padding: 0}"
+        class="post"
+    >
+        <header slot="header" class="post__header">
+            <h3>Post title</h3>
+            <small>
+                <i class="el-icon-time "></i>
+                {{ new Date().toLocaleString() }}
+            </small>
+        </header>
 
-    </article>
+        <div class="post__body">
+            <img 
+                src="https://netmaddy.com/wp-content/uploads/2017/02/qbeats-blog-post-how-does-qbeats-integrate-with-wordpress-5.jpg"
+                alt="Notebook image"
+                class="post__image"
+            /> 
+        </div>
+
+        <footer class="post__footer">
+            <!-- Если указываем атрибут и не указываем свойство, то это значит, что мы передаем его значение как true -->
+            <!-- На днях только это узнал/осознал, поэтому для закрепления запишу еще раз сюда -->
+            <el-button round @click="openPost">Open</el-button>
+
+            <span>
+                <i class="el-icon-message"></i>
+                5
+            </span>
+        </footer>
+    </el-card>
 </template>
 
-<script>
+<script> 
 export default {
-    
+    methods: {
+        openPost() {
+            console.log('Open post ');
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     .post {
-        height: 200px;
-        border: 1px solid black;
-        margin-bottom: 2rem;
+        margin-bottom: 1.5rem; 
+
+        &__header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        &__image {
+            width: 100%;
+            height: auto ;
+        } 
+
+        &__footer {
+            padding: 1rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
     }
 </style>
