528ebef38bd79d54aa595334c3fa9ab372ce246f
lightxlamp <sheveliukhin.s@gmail.com>
2020-05-16 12:24:35 +0600

    feat: Add "Login" page. Add global a margin class to global CSS

---
+++ pages/admin/login.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-card
+    shadow="always"
+    :style="{width: '500px'}"
+  >
+    <el-form
+      :model="controls"
+      :rules="rules"
+      ref="form"
+      @submit.native.prevent="onSubmit"
+    >
+      <h2>Enter to admin panel</h2>
+
+      <el-form-item label="Login" prop="login">
+        <el-input v-model.trim="controls.login" />
+      </el-form-item>
+
+      <div class="mb2">
+        <el-form-item label="Password" prop="password">
+          <el-input v-model.trim="controls.password" type="password" />
+        </el-form-item>
+      </div>
+
+      <el-form-item>
+        <el-button
+          type="primary"
+          native-type="submit"
+          round
+          :loading="loading"
+        >
+          Login
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      loading: false,
+      controls: {
+        login: '',
+        password: ''
+      },
+      rules: {
+        login: [
+          { required: true, message: 'Enter login', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: 'Enter password', trigger: 'blur' },
+          { min: 6, message: 'Min length for password is 6 characters', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit')
+    }
+  }
+}
+</script>
