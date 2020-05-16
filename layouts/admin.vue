bd8a3ed3b26b285216c01c18633e87b682776d11
lightxlamp <sheveliukhin.s@gmail.com>
2020-05-16 11:38:50 +0600

    feat: add "admin" layout

---
+++ layouts/admin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrapper">
+        <el-container :style="{height: '100%'}">
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>      
+                <nuxt />
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .wrapper {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
